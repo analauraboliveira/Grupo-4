5521c02e31b6df500175abedbee673ab096a6a6b
Gabriel <rocha_gabriel@id.uff.br>
2026-04-11 12:31:48 -0300

    Adicionando as rotas de atendenteController

--- back-template/src/router.ts
+++ back-template/src/router.ts
@@ -1,12 +1,20 @@
 import { Router } from "express";
 import SolicitanteController from "./controllers/solicitanteController";
+import { AtendenteController } from './controllers/AtendenteController';
+
 const router = Router();
 
 const solicitanteController = new SolicitanteController();
+const atendenteController = new AtendenteController();
 
 router.post('/calls', solicitanteController.newCall);
 router.get('/calls/:id', solicitanteController.getCallById);
 router.put('/calls/:id', solicitanteController.updateCall);
 router.get('/calls', solicitanteController.getHistory);
+
+router.get('/chamados/:id/historico', atendenteController.getHistory);
+router.patch('/chamados/:id/assumir', atendenteController.assignTicket);
+router.patch('/chamados/:id/status', atendenteController.updateStatus);
+router.post('/chamados/:id/comentarios', atendenteController.updateComment);
 
 export default router;
